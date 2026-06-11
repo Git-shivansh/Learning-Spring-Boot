c2a4e83da31682471768bea895038078ac342583
Shivansh Tiwari <stshivansh354@gmail.com>
2026-06-11 21:19:22 +0530

    commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,12 +1,28 @@
 package com.example.demo.controller;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.LoginRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @RestController
 @RequestMapping("/api")
 public class AuthController {
+    
+    @GetMapping("/sign-up")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+
+    @GetMapping("/login")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
     
     
     
